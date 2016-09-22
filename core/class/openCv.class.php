d181a2fe3eceba8cbd75410779043ec53cbc64d5
Halbout Michael <michaelhalbout@gmail.com>
2016-09-22 11:10:02 +0200

    Update openCv.class.php

--- core/class/openCv.class.php
+++ core/class/openCv.class.php
@@ -80,9 +80,9 @@ class openCv extends eqLogic {
 		$return['log'] = 'openCv_update';
 		$return['progress_file'] = '/tmp/compilation_openCv_in_progress';
 		$br = (php_sapi_name() == "cli")? "":"<br>";
-		if(!extension_loaded('opencv')) {
+	/*	if(!extension_loaded('opencv')) {
 			dl('opencv.' . PHP_SHLIB_SUFFIX);
-		}
+		}*/
 		$function = 'confirm_' . $module . '_compiled';
 		if (extension_loaded($module)) 
 			$return['state'] = 'ok';
