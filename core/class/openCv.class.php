4ea83cd14d3eb4d3a1d2c9599af874be6b066040
Halbout Michael <michaelhalbout@gmail.com>
2016-09-22 09:02:04 +0200

    Update openCv.class.php

--- core/class/openCv.class.php
+++ core/class/openCv.class.php
@@ -49,10 +49,16 @@ class openCv extends eqLogic {
 		$return = array();
 		$return['log'] = 'openCv_update';
 		$return['progress_file'] = '/tmp/compilation_openCv_in_progress';
-		if(file_exists('/usr/local/src/openCv/'))
+		$br = (php_sapi_name() == "cli")? "":"<br>";
+		if(!extension_loaded('opencv')) {
+			dl('opencv.' . PHP_SHLIB_SUFFIX);
+		}
+		$function = 'confirm_' . $module . '_compiled';
+		if (extension_loaded($module)) 
 			$return['state'] = 'ok';
-		else
+		 else 
 			$return['state'] = 'nok';
+		
 		return $return;
 	}
 	public static function dependancy_install() {
@@ -85,7 +91,39 @@ class openCv extends eqLogic {
 	}
 }
 class openCvCmd extends cmd {
-  
+	use OpenCV\Capture as Capture;
+	use OpenCV\Image as Image;
+	use OpenCV\Histogram as Histogram;
 	public function execute($_options = array()) {
+		switch($this->getLogicalId()){
+			case "JpegCapture":
+				/* Test the face detectoin feature, using a capture from the camera */
+				$capture = Capture::createCameraCapture(0);
+				$image = $capture->queryFrame();
+				$result = $image->haarDetectObjects("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml");
+				foreach ($result as $r) {
+					$image->rectangle($r['x'], $r['y'], $r['width'], $r['height']);
+				}
+				//$image = $image->convertColor(7);
+				$image->save('/tmp/camera.jpg');
+			
+			break;
+			case "MovieCapture":
+				$capture = OpenCV\Capture::createFileCapture('movie.avi');
+				$image = $capture->queryFrame();
+				$result = $image->haarDetectObjects("data/haarcascades/haarcascade_frontalface_default.xml");
+				foreach ($result as $r) {
+					$image->rectangle($r['x'], $r['y'], $r['width'], $r['height']);
+				}
+				$image->save('/tmp/video.jpg');
+			break;
+			case "edgeDetect":
+				$i = Image::load("test.jpg", Image::LOAD_IMAGE_COLOR);
+				$dst = $i->sobel(1, 0, 1);
+				$dst->save("test_sobel.jpg");
+				$dst2 = $i->canny(10, 50, 3);
+				$dst2->save("test_canny.jpg");
+			break;
+		}
 }
 ?>
