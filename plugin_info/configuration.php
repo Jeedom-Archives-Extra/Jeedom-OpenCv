d19a53eaf81073d1fef27ad4ec2647fc2278b26c
Halbout Michael <michaelhalbout@gmail.com>
2017-10-04 10:07:36 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -7,113 +7,90 @@ if (!isConnect()) {
     die();
 }
 ?>
-<div class="col-sm-6">
-	<form class="form-horizontal">
-		<legend>Général</legend>
-		<fieldset>
-			<div class="form-group">
-                		<label class="col-lg-4 control-label">{{Commande d'alerte (mail, slack...)}}</label>
-                		<div class="col-lg-4">
-					<div class="input-group">
-						<input type="text" class="configKey" data-l1key="alertMessageCommand" placeholder="{{Commande mail pour l'envoi d'une capture}}"/>
-						<span class="input-group-btn">
-							<a class="btn btn-default listCmdActionMessage" id="bt_selectActionMessage"><i class="fa fa-list-alt"></i></a>
-						</span>
-					</div>
-				</div>      
-			</div>   
-			<div class="form-group">    
-				<label class="col-lg-4 control-label">Nombre de captures</label>
-				<div class="col-lg-4">
-					<input type="text" class="configKey" data-l1key="NbSnap">
-				</div>
-			</div>
-		</fieldset>
-	</form>
-</div>
-<div class="col-sm-6">
-	<form class="form-horizontal">
-		<legend>Configuration du demon Alprd</legend>
-		<fieldset>
-			<div class="form-group">
-				<label class="col-lg-4 control-label">{{Repertoire Snapshot}}</label>
-				<div class="col-lg-4">
-					<input type="text" class="configKey" data-l1key="SnapshotFolder">
-				 </div>
-			</div> 
-			<div class="form-group">
-				<label class="col-lg-4 control-label">{{Activer le Snapshot}}</label>
-				<div class="col-lg-4">
-					<input type="checkbox" class="configKey" data-label-text="{{Activer}}" data-l1key="snapshot"/>
-				 </div>
-			</div> 
-		</fieldset>
-		<legend>Configuration des Camera<a class="btn btn-success btn-xs pull-right cursor" id="bt_AddCamera"><i class="fa fa-check"></i> {{Ajouter}}</a></legend>
-		<fieldset>
-			<div class="form-group">
-				<label>{{Liste des camera}}</label>
-				<div class="CameraListe"></div>
-			</div> 
-		</fieldset>
-	</form>
-</div>
-<script>
+<form class="form-horizontal">
+	<legend>Configuration des Camera<a class="btn btn-success btn-xs pull-right cursor" id="bt_AddCamera"><i class="fa fa-check"></i> {{Ajouter}}</a></legend>
+	<fieldset>
+		<table id="table_camera" class="table table-bordered table-condensed tablesorter">
+			<thead>
+				<tr>
+					<th>{{Nom}}</th>
+					<th>{{Url}}</th>
+					<th>{{}}</th>
+				</tr>
+			</thead>
+			<tbody></tbody>
+		</table>
+	</fieldset>
+</form>
+<script>	
 $.ajax({
 	type: "POST",
-	timeout:8000, 
+	timeout:8000,
 	url: "core/ajax/config.ajax.php",
 	data: {
 		action:'getKey',
 		key:'{"configuration":""}',
-		plugin:'openCv',
+		plugin:'openCv'
 	},
 	dataType: 'json',
 	error: function(request, status, error) {
 		handleAjaxError(request, status, error);
 	},
-	success: function(data) { 
+	success: function(data) {
 		if (data.state != 'ok') {
 			$('#div_alert').showAlert({message: data.result, level: 'danger'});
 			return;
 		}
 		if (data.result['configuration']!=''){
-			$.each(data.result['configuration'], function( key, value ){
-				AddCamera($('.CameraListe'),key);	
-				$('.configKey[data-l1key=configuration][data-l2key='+key+'][data-l3key=cameraUrl]').val(data.result['configuration'][key]['cameraUrl']);
-				});
+			var Camera= new Object();
+			$.each(data.result['configuration'], function(param,valeur){
+				switch(typeof(valeur)){
+					case 'object':
+						$.each(valeur, function(key,value ){
+							if (typeof(Camera[key]) === 'undefined')
+								Camera[key]= new Object();
+							if (typeof(Camera[key]['configuration']) === 'undefined')
+								Camera[key]['configuration']= new Object();
+							Camera[key]['configuration'][param]=value;
+						});
+					break;
+					case 'string':
+						if (typeof(Camera[0]) === 'undefined')
+							Camera[0]= new Object();
+						if (typeof(Camera[0]['configuration']) === 'undefined')
+							Camera[0]['configuration']= new Object();
+						Camera[0]['configuration'][param]=valeur;
+					break;
+				}
+			});
+			$.each(Camera, function(id,data){
+				AddCamera($('#table_camera tbody'),data);
+			});
 		}
 	}
-});	
-$("#bt_selectActionMessage").on('click', function () {
-    jeedom.cmd.getSelectModal({cmd: {type: 'action',subType : 'message'}}, function (result) {
-        $(".configKey[data-l1key=alertMessageCommand]").atCaret('insert',result.human);
-    });
 });
-
-$('#bt_AddCamera').on('click',function(){
-	var cameraNb= $('div.camera').length+1;
-	var cameraId='camera_'+cameraNb;
-	AddCamera($(this).closest('.form-horizontal').find('.CameraListe'),cameraId);
+$('body').on('click','.bt_removecamera', function() {
+	$(this).closest('tr').remove();
 });
-function openCv_postSaveConfiguration(){
-};
-function AddCamera(div,cameraId){
-	div.append($('<div class="camera">')
-		.append($('<div class="form-group">')
-			.append($('<label class="col-lg-4 control-label">').text('{{Url de la Camera}}'))
-			.append($('<div class="col-lg-4 ">')
-				.append($('<input class="configKey form-control" data-l1key="configuration" data-l2key="'+cameraId+'" data-l3key="cameraUrl">'))))
-		.append('<legend></legend>')
-		.append($('<div class="form-group">')
-			.append($('<label class="col-lg-4 control-label">').text('{{Supprimer cette camera}}'))
-			.append($('<div class="col-lg-4">')
-				.append($('<a class="btn btn-danger" id="bt_removecamera">')
-					.append($('<i class="fa fa-check">'))
-					.text('{{Supprimer}}'))))
-		.append('<legend></legend>'));
-	$('body').on('click','#bt_removecamera', function() {
-		$(this).closest('.camera').remove();
-	}); 
-}
-
+$('body').on('click','#bt_AddCamera', function() {
+	AddCamera($('#table_camera tbody'),'');
+});
+function AddCamera(_el,data){
+	var id= $('.configKey[data-l1key=configuration][data-l2key=cameraUrl]').length +1;
+	var tr=$('<tr>');
+	tr.append($('<td>')
+		.append($('<input class="configKey form-control input-sm "data-l1key="configuration" data-l2key="name">')));
+	tr.append($('<td>')
+		.append($('<input type="text" class="configKey form-control" data-l1key="configuration" data-l2key="username" placeholder="{{Nom d\'utilisateur}}"/>'))
+		.append($('<input type="password" class="configKey form-control" data-l1key="configuration" data-l2key="password" placeholder="{{Mot de passe}}"/>'))
+		.append($('<input type="text" class="configKey form-control" data-l1key="configuration" data-l2key="cameraUrl" placeholder="{{URL de capture}}"/>')));
+	tr.append($('<td>')
+		.append($('<input type="hidden" class="configKey" data-l1key="configuration" data-l2key="id">'))
+		.append($('<span class="input-group-btn">')
+			.append($('<a class="btn btn-default btn-sm bt_removecamera">')
+				.append($('<i class="fa fa-minus-circle">')))));
+	_el.append(tr);
+	_el.find('tr:last').setValues(data, '.configKey');
+	_el.find('tr:last').find('.configKey[data-l1key=configuration][data-l2key=id]').val(id);
+} 
 </script>
