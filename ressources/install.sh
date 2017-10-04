7d278f8f8159e78956ae3c54531a852da2eeaceb
Halbout Michael <michaelhalbout@gmail.com>
2017-10-04 10:45:19 +0200

    Update install.sh

--- ressources/install.sh
+++ ressources/install.sh
@@ -7,17 +7,18 @@ echo "**************************************************************************
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 echo 10 > /tmp/compilation_openCv_in_progress
-sudo apt-get install -y --force-yes autoconf automake libtool
-sudo apt-get install -y --force-yes build-essential
-sudo apt-get install -y --force-yes cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y --force-yes python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y --allow autoconf automake libtool
+sudo apt-get install -y --allow build-essential
+sudo apt-get install -y --allow cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y --allow python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 echo 40 > /tmp/compilation_openCv_in_progress
 sudo rm -R /usr/local/src/openCv/
 sudo mkdir /usr/local/src/openCv/
+sudo chmod -R 777 /usr/local/src/openCv/
 echo "*****************************************************************************************************"
 echo "*                                            Compile OpenCV:                                        *"
 echo "*****************************************************************************************************"
-sudo apt-get install -y --force-yes  libopencv-dev python-opencv
+sudo apt-get install -y --allow  libopencv-dev python-opencv
 echo "*****************************************************************************************************"
 echo "*                                        Compile OpenCV-for-PHP:                                    *"
 echo "*****************************************************************************************************"
